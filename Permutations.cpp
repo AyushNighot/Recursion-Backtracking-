eebdec4ce4494f40512e0edd222b3c230147ac5f
Ayush Nighot <68859871+AyushNighot@users.noreply.github.com>
2024-01-31 16:01:27 +0530

    Permutations.cpp

---
+++ Permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    private:
+    void permute(vector<vector<int>>&ans ,vector<int>&nums,int index)
+    {
+        //base case
+        if (index == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i = index ; i < nums.size() ;i++)
+        {
+            swap(nums[index],nums[i]);
+            permute(ans,nums,index+1);
+            swap(nums[index],nums[i]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+            vector<vector<int>>ans;
+            permute(ans,nums,0);  
+            return ans;  
+    }
+};
